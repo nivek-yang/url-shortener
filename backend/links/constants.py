0dc0dda481e70b2e7866bb945f799fb27329cdd4
KWY <nivekyang12014@gmail.com>
2025-06-27 00:55:15 +0800

    feat: add error messages for URL, slug, and password validations in Link model

---
+++ backend/links/constants.py
@@ -0,0 +1,4 @@
+SLUG_MAX_LENGTH_ERROR = 'Slug exceeds maximum length of 50 characters.'
+SLUG_DUPLICATE_ERROR = 'The slug is already in use. Please choose a different one.'
+URL_INVALID_ERROR = 'Please enter a valid URL.'
+PASSWORD_MAX_LENGTH_ERROR = 'Password exceeds maximum length of 64 characters.'
