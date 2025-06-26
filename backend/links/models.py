36b463d42ee831111b5d383c383283e4ff1dd35b
KWY <nivekyang12014@gmail.com>
2025-06-27 00:55:15 +0800

    feat: add error messages for URL, slug, and password validations in Link model

--- backend/links/models.py
+++ backend/links/models.py
@@ -2,32 +2,57 @@ from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.text import slugify
 
+from .constants import (
+    PASSWORD_MAX_LENGTH_ERROR,
+    SLUG_DUPLICATE_ERROR,
+    SLUG_MAX_LENGTH_ERROR,
+    URL_INVALID_ERROR,
+)
 from .utils import generate_unique_slug
 
 
 class Link(models.Model):
-    original_url = models.URLField(max_length=2048)
-    slug = models.SlugField(max_length=50, unique=True, blank=True, db_index=True)
-    password = models.CharField(max_length=64, blank=True, null=True)
+    original_url = models.URLField(
+        max_length=2048, error_messages={'invalid': URL_INVALID_ERROR}
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        blank=True,
+        db_index=True,
+        error_messages={
+            'unique': SLUG_DUPLICATE_ERROR,
+            'max_length': SLUG_MAX_LENGTH_ERROR,
+        },
+    )
+    password = models.CharField(
+        max_length=64,
+        blank=True,
+        null=True,
+        error_messages={'max_length': PASSWORD_MAX_LENGTH_ERROR},
+    )
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     expires_at = models.DateTimeField(blank=True, null=True)
 
+    def clean_slug(self, slug_value):
+        slugified_slug = slugify(slug_value)
+        if len(slugified_slug) > 50:
+            raise ValidationError(SLUG_MAX_LENGTH_ERROR)
+        if Link.objects.exclude(pk=self.pk).filter(slug=slugified_slug).exists():
+            raise ValidationError(SLUG_DUPLICATE_ERROR)
+        return slugified_slug
+
     def save(self, *args, **kwargs):
-        if self.slug:  # User provided a custom slug
-            slugified_slug = slugify(self.slug)
+        if self.slug:
+            self.slug = self.clean_slug(self.slug)
 
-            if Link.objects.exclude(pk=self.pk).filter(slug=slugified_slug).exists():
-                raise ValidationError(
-                    f"The slug '{slugified_slug}' is already in use. Please choose a different one."
-                )
-
-            self.slug = slugified_slug
-
-        else:  # No custom slug provided, generate one
+        else:
             self.slug = generate_unique_slug()
             while Link.objects.exclude(pk=self.pk).filter(slug=self.slug).exists():
                 self.slug = generate_unique_slug()
+
+        self.full_clean()
         super().save(*args, **kwargs)
 
     def __str__(self):
