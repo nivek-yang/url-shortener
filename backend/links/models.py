b7ca7b87bcba299b17bc985cdc09aa62449dd995
KWY <nivekyang12014@gmail.com>
2025-06-29 12:34:08 +0800

    feat: enhance Link model with click count and password validation

--- backend/links/models.py
+++ backend/links/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.text import slugify
@@ -26,7 +27,7 @@ class Link(models.Model):
         },
     )
     password = models.CharField(
-        max_length=64,
+        max_length=128,
         blank=True,
         null=True,
         error_messages={'max_length': PASSWORD_MAX_LENGTH_ERROR},
@@ -34,6 +35,7 @@ class Link(models.Model):
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     expires_at = models.DateTimeField(blank=True, null=True)
+    click_count = models.PositiveIntegerField(default=0)
 
     def clean_slug(self, slug_value):
         slugified_slug = slugify(slug_value)
@@ -43,6 +45,11 @@ class Link(models.Model):
             raise ValidationError(SLUG_DUPLICATE_ERROR)
         return slugified_slug
 
+    def clean_password(self, password_value):
+        if password_value and len(password_value) > 64:
+            raise ValidationError(PASSWORD_MAX_LENGTH_ERROR)
+        return make_password(password_value) if password_value else None
+
     def save(self, *args, **kwargs):
         if self.slug:
             self.slug = self.clean_slug(self.slug)
@@ -51,6 +58,9 @@ class Link(models.Model):
             self.slug = generate_unique_slug()
             while Link.objects.exclude(pk=self.pk).filter(slug=self.slug).exists():
                 self.slug = generate_unique_slug()
+
+        if self.password:
+            self.password = self.clean_password(self.password)
 
         self.full_clean()
         super().save(*args, **kwargs)
