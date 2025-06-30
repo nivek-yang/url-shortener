1d661dbfabdf33bb271119f8431496f2dd8f7e43
KWY <nivekyang12014@gmail.com>
2025-06-30 18:24:17 +0800

    feat: add CRUD for links

--- backend/links/urls.py
+++ backend/links/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     path('<slug:slug>/', views.redirect_link, name='redirect_link'),
     path('links/new/', views.new, name='new'),
     path('api/fetch-page-info/', views.fetch_page_info_api, name='fetch_page_info_api'),
+    path('links/list', views.index, name='index'),
+    path('links/update/<int:id>/', views.update, name='update'),
+    path('links/delete/<int:id>/', views.delete, name='delete'),
 ]
