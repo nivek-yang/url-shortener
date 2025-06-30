c5f8199be732304f657acd57d889024c7c85b7b2
KWY <nivekyang12014@gmail.com>
2025-06-30 09:25:31 +0800

    feat: add URL shortener service page with Alpine.js handling shortener API

--- backend/links/urls.py
+++ backend/links/urls.py
@@ -9,4 +9,5 @@ app_name = 'links'
 urlpatterns = [
     path('api/links/shortener', views.link_shortener_api, name='link_shortener_api'),
     path('<slug:slug>/', views.redirect_link, name='redirect_link'),
+    path('links/new/', views.new, name='new'),
 ]
