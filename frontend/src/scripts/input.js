2453f86a8af70fcaa1c82f2737036df56e1a7506
KWY <nivekyang12014@gmail.com>
2025-06-30 18:25:34 +0800

    feat: initialize pages app and add templates for links management

--- frontend/src/scripts/input.js
+++ frontend/src/scripts/input.js
@@ -1,6 +1,8 @@
 import Alpine from 'alpinejs';
+import { indexShortener } from './indexShortener.js';
 import { shortenerForm } from './shortenerForm.js';
 
 Alpine.data('shortenerForm', shortenerForm);
+Alpine.data('indexShortener', indexShortener);
 
 Alpine.start();
