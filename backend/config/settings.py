b1e77534007337169391610d31bd30a9be768ffc
KWY <nivekyang12014@gmail.com>
2025-06-30 19:42:13 +0800

    fix: fix problems in GCP server

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR.parent / 'frontend' / 'templates'],
+        'DIRS': [os.path.join('/app', 'frontend', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
